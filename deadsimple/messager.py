5b86b2750764922e660e3c6d421606f8e493030f
Leonard Lewis <leonardlewis@Leonards-MacBook-Pro.local>
2019-01-19 12:11:03 -0800

    Create Twilio messenger.

---
+++ deadsimple/messager.py
@@ -0,0 +1,7 @@
+# Configure a Cron job to send a workout message every day at 8am.
+
+# Send a message to the user: "You're ready to workout! Reply "Y" to start or
+# N to cancel.
+
+# Receive response from user and reply "Your first exercise is X" or "OK, see you
+# tomorrow!"
